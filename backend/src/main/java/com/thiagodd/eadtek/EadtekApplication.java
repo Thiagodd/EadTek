3e8ea239548dbb8585882fc86faf24db20587693
Thiagodd <=>
2022-12-05 11:53:13 -0300

    Backend project created

---
+++ backend/src/main/java/com/thiagodd/eadtek/EadtekApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodd.eadtek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadtekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadtekApplication.class, args);
+	}
+
+}
